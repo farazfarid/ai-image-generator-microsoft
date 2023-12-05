4fc3874183f92274e566b4920fbaaf74e76353c6
Fabian Faraz Farid <63512528+farazfarid@users.noreply.github.com>
2023-12-05 04:16:39 +0100

    Update route.ts

--- app/api/generateImage/route.ts
+++ app/api/generateImage/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const res = await request.json();
   const prompt = res.prompt;
-
+  export const dynamic = 'force-dynamic';
   const response = await fetch(
     "https://ai-image-generator-microsoft-app.azurewebsites.net/api/generateimage",
     {
